ab77d9b1346cb704dea655dd47d968715818b8d9
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-07-04 19:15:52 -0300

    formulario adicionado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Resume from './components/Resume';
+import Form from './components/Form'
 
 import GlobalStyle from './styles/global'
 
@@ -9,6 +10,7 @@ const App = () => {
         <>
             <Header />
             <Resume />
+            <Form/>
             <GlobalStyle />
         </>
     )
