52439653b395a656dac1539dd05c13d1f134b26e
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-07-04 21:57:54 -0300

    troca de svgs

--- src/components/GridItem/index.js
+++ src/components/GridItem/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import * as C from './style';
+import React from "react";
+import * as C from "./styles";
 import {
-    FaRegArrowAltCircleUp,
-    FaRegArrowAltCircleDown,
-    FaTrash,
-} from 'react-icons/fa'
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaTrash,
+} from "react-icons/fa";
 
 const GridItem = ({ item, onDelete }) => {
-    return (
-        <C.Tr>
-            <C.Td>{item.desc}</C.Td>
-            <C.Td>{item.amount}</C.Td>
-            <C.Td alignCenter>
-                {item.expense ? (
-                    <FaRegArrowAltCircleDown color='red' />
-                ) : (
-                    <FaRegArrowAltCircleUp color='green' />
-                )}
-            </C.Td>
-            <C.Td alignCenter>
-                    <FaTrash onclick={() => onDelete(item.id)} />
-            </C.Td>
-        </C.Tr>
-    );
+  return (
+    <C.Tr>
+      <C.Td>{item.desc}</C.Td>
+      <C.Td>{item.amount}</C.Td>
+      <C.Td alignCenter>
+        {item.expense ? (
+          <FaRegArrowAltCircleDown color="red" />
+        ) : (
+          <FaRegArrowAltCircleUp color="green" />
+        )}
+      </C.Td>
+      <C.Td alignCenter>
+        <FaTrash onClick={() => onDelete(item.id)} />
+      </C.Td>
+    </C.Tr>
+  );
 };
 
-export default GridItem
+export default GridItem;
